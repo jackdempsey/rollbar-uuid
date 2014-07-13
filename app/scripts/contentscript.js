238543c2c7509bf5ad9d5b71f265f63332c12ab1
Jack Dempsey <jack@instacart.com>
2014-07-12 20:50:55 -0700

    look for rollbar-uuid in meta tag

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var uuidElement = document.getElementById('rollbar-uuid');
+var uuid = document.querySelector('meta[name="rollbar-uuid"]').attributes.value.value
 
-if (uuidElement) {
-  var uuid = uuidElement.innerHTML.substr(7);
-
+if (uuid) {
+  //give Rollbar a few seconds for the uuid to propagate through their system.
+  //sometimes you'll still see this message and just refresh to clear it:
+  //"UUID not found. It may still be processing or it could have been deleted. Hit refresh?"
   setTimeout(function() {
     window.location.href = "https://rollbar.com/item/uuid/?uuid=" + uuid
   }, 3000);
